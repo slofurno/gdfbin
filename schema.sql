3cb670f0b692a12d7b23d183d07d90860c1250f6
slofurno <slofurno@gmail.com>
2016-02-23 18:43:10 -0500

    account emails must be unique

--- schema.sql
+++ schema.sql
@@ -2,4 +2,4 @@ CREATE TABLE pastes (id text, content text, time number);
 CREATE TABLE logins (account text, token text);
 CREATE TABLE accounts (id text, email text, password text);
 CREATE TABLE bookmarks (account text, paste text, name text);
-
+CREATE UNIQUE INDEX account_email on accounts(email);
